65e21ef543a0f40df4d436b7ce2ca8b3af4f8598
michael <mgebregziabher86@gmail.com>
2017-10-06 10:08:36 -0500

    -completed with bonus which is formatting phone number

--- src/util/FileHandler.java
+++ src/util/FileHandler.java
@@ -1,44 +1,36 @@
 package util;
-
 import java.nio.file.*;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class FileHandler {
+    public class FileHandler {
+        private String directory;
+        private String fileName;
+        private Path path;
 
-    private String directory;
-    private String fileName;
-    private Path path;
-
-    public FileHandler(String directory, String fileName) throws IOException {
-        this.directory = directory;
-        this.fileName = fileName;
-        this.createFile();
-    }
-
-    private void createFile() throws IOException {
-        this.path = Paths.get(this.directory, this.fileName); // files
-
-        if (!Files.exists(path.getParent())) {
-            Files.createDirectory(path.getParent());
+        public FileHandler(String directory, String fileName) throws IOException {
+            this.directory = directory;
+            this.fileName = fileName;
+            this.createFile();
         }
-        if (!Files.exists(path)) {
-            Files.createFile(path);
+        private void createFile() throws IOException {
+            this.path = Paths.get(this.directory, this.fileName); // files
+            if (!Files.exists(path.getParent())) {
+                Files.createDirectory(path.getParent());
+            }
+            if (!Files.exists(path)) {
+                Files.createFile(path);
+            }
         }
-    }
-
-    public List<String> readAllContents() throws IOException {
-        return Files.readAllLines(this.path);
-    }
-
-    public void writeToFile(List<String> contents, boolean append) throws IOException {
-        if(append){
-            Files.write(this.path, contents, StandardOpenOption.APPEND);  // write several lines to a file
-        } else {
-            Files.write(this.path, contents);
+        public List<String> readAllContents() throws IOException {
+            return Files.readAllLines(this.path);
+        }
+        public void writeToFile(List<String> contents, boolean append) throws IOException {
+            if(append){
+                Files.write(this.path, contents, StandardOpenOption.APPEND);  // write several lines to a file
+            } else {
+                Files.write(this.path, contents);
+            }
         }
     }
-
-
-}
